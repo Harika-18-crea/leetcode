8944e5797d1c788635c5494272ec71108e5c035e
Harika-18-crea <harikakeshetti7783@gmail.com>
2026-06-18 10:56:32 +0530

    Time: 2 ms (99.39%) | Memory: 47 MB (57.54%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i]+nums[j]==target) {
-                    return new int[] { i, j };
-                }
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int m=target-nums[i];
+            if(hm.containsKey(m)){
+                return new int[] {hm.get(m),i};
             }
+            hm.put(nums[i],i);
         }
-        return new int[] {};
+        return new int []{};
     }
 }
